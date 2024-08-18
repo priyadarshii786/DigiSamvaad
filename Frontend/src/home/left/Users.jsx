c868f284366bfee6fd5b710894df88a75cabccad
priyadarshii786 <tathagatp918@gmail.com>
2024-08-19 05:00:44 +0530

    C-02

--- Frontend/src/home/left/Users.jsx
+++ Frontend/src/home/left/Users.jsx
@@ -4,6 +4,8 @@ import User from "./User";
 function Users() {
   return (
     <div style={{ maxHeight: "calc(92vh)" }} className="overflow-y-auto">
+      {" "}
+      {/**this styling in div is introduced to make the scroll bar visible and to be able to use it. */}
       <User />
       <User />
       <User />
